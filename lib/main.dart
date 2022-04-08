555a45b6c1b088e5c4b941b722d609ce3eeebba2
bawar Khalid <bawarkhalid912@gmail.com>
2022-04-08 13:28:42 +0300

    commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  Widget build(BuildContext context){
+    return MaterialApp(
+      title: 'flutter app',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Flutter App"),
+      ),
+      body: Center(
+        child: Text('widget playground'),
+      ),
+    );
+  }
+}
