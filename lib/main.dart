a62455343f1b453baf7e35b67e7118d29ad074c0
bawar Khalid <bawarkhalid912@gmail.com>
2022-04-15 14:03:44 +0300

    added flat button

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:expense/transaction.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
-void main(){
+void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget{
-  Widget build(BuildContext context){
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context) {
     return MaterialApp(
       title: 'flutter app',
       home: MyHomePage(),
@@ -16,83 +18,102 @@ class MyApp extends StatelessWidget{
 }
 
 class MyHomePage extends StatelessWidget {
-
   final List<Transaction> transactions = [
     Transaction(
       id: 't1',
-       title: 'new shoes',
-        amount: 20.1,
-         date: DateTime.now(),
-         ),
-         Transaction(id: 't5',
-          title: 'shirt', 
-          amount: 59.01, 
-          date: DateTime.now(),
-          ),
+      title: 'new shoes',
+      amount: 20.1,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't5',
+      title: 'shirt',
+      amount: 59.01,
+      date: DateTime.now(),
+    ),
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Flutter App demo for github"),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Card(
-             color: Colors.red,
-            child: Container(
-              height: 40,
-              width: 40,
-              child: Text("this is Chart")
-              ),
-          ),
-          Column(
-            children: transactions.map((tx){
-              return Card(
-                child: Row(
+        appBar: AppBar(
+          title: Text("Flutter App demo for github"),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Card(
+              color: Colors.red,
+              child: Container(
+                  height: 40, width: 40, child: Text("this is Chart")),
+            ),
+            Card(
+              elevation: 5,
+              child: Container(
+                padding: EdgeInsets.all(10),
+                child: Column(
                   children: [
-                    Container(
-                   margin: EdgeInsets.symmetric(
-                     vertical: 10, horizontal: 15
-                     ),
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.purple, width: 2),
-                      ),
-                      padding: EdgeInsets.all(10),
-                      child: Text(
-                        '\$${tx.amount}' ,
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                          color: Colors.purple,
-                        ), 
-                        ),
+                    TextField(
+                      decoration: InputDecoration(label: Text('title')),
                     ),
-                    Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(tx.title, style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16,
-                        ),),
-                        Text(
-                        DateFormat.yMd().format(tx.date),
-                          style: TextStyle(
-                          color: Colors.grey,
-                        ),)
-
-                      ],
+                    TextField(
+                      decoration: InputDecoration(label: Text('date')),
+                    ),
+                    FlatButton(
+                      child: Text("Add Transaction"),
+                      textColor: Colors.purple,
+                      onPressed: (() {}),
                     )
                   ],
                 ),
-              );
-            }).toList(),
-          )
-        ],
-      )
-    );
+              ),
+            ),
+            Column(
+              children: transactions.map((tx) {
+                return Card(
+                  child: Row(
+                    children: [
+                      Container(
+                        margin:
+                            EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.purple, width: 2),
+                        ),
+                        padding: EdgeInsets.all(10),
+                        child: Text(
+                          '\$${tx.amount}',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.purple,
+                          ),
+                        ),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            tx.title,
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                            ),
+                          ),
+                          Text(
+                            DateFormat.yMd().format(tx.date),
+                            style: TextStyle(
+                              color: Colors.grey,
+                            ),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                );
+              }).toList(),
+            ),
+          ],
+        ));
   }
 }
